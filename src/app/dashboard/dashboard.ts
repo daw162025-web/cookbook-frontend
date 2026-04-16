fd291e1935668db5d236cb600e04e7b91ebf86bb
Telma <daw16.2025@gmail.com>
2026-04-16 17:32:31 +0200

    empezando con el panel de admin, guard, service...

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { AdminService } from '../services/admin.service';
 import { CommonModule } from '@angular/common';
 
@@ -10,13 +10,14 @@ import { CommonModule } from '@angular/common';
 })
 export class DashboardComponent implements OnInit {
   private adminService = inject(AdminService);
+  private cdr = inject(ChangeDetectorRef); 
   stats: any = null;
 
   ngOnInit() {
     this.adminService.getStats().subscribe({
       next: (data) => {
         this.stats = data;
-        console.log('Estadísticas cargadas:', data);
+        this.cdr.detectChanges(); 
       },
       error: (err) => console.error('Error al cargar stats', err)
     });
