572deae023fa02242eb516b277bed338b12f5671
Telma <daw16.2025@gmail.com>
2026-04-20 12:04:32 +0200

    implementar gestion de recetas en dashboard

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api'
-  // apiUrl: 'https://cookbook-backend-production-9c68.up.railway.app/api'
+  // apiUrl: '/api'
+  apiUrl: 'https://cookbook-backend-production-9c68.up.railway.app/api'
 };
